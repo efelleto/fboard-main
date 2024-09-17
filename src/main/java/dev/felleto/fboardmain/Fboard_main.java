6ed17a7bb4c064d6ad1c22bb3d83507cc1ed7935
efelleto <eduardofelleto@icloud.com>
2024-09-16 22:44:47 -0300

    [chore] correção na classe main

--- src/main/java/dev/felleto/fboardmain/Fboard_main.java
+++ src/main/java/dev/felleto/fboardmain/Fboard_main.java
@@ -7,15 +7,11 @@ public final class Fboard_main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(" &7-----------------------------------------\n");
-        Bukkit.getConsoleSender().sendMessage(" &f| &afBoard Plugin\n");
-        Bukkit.getConsoleSender().sendMessage(" &f| Author: &aEduardo Felleto");
-        Bukkit.getConsoleSender().sendMessage(" &f| Version: &a1.0.0\n");
-        Bukkit.getConsoleSender().sendMessage(" &7-----------------------------------------\n");
+        Bukkit.getConsoleSender().sendMessage("[fBoard] Plugin iniciado");
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage(" &cfBoard desligado!\n");
+        Bukkit.getConsoleSender().sendMessage("[fBoard] Plugin desligado");
     }
 }
