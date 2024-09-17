c4f565777705f01689813c85a9be7c5e6d7d0d40
efelleto <eduardofelleto@icloud.com>
2024-09-16 22:33:27 -0300

    [chore] mensagens de ativação do plugin

--- src/main/java/dev/felleto/fboardmain/Fboard_main.java
+++ src/main/java/dev/felleto/fboardmain/Fboard_main.java
@@ -1,17 +1,21 @@
 package dev.felleto.fboardmain;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Fboard_main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getConsoleSender().sendMessage(" &7-----------------------------------------\n");
+        Bukkit.getConsoleSender().sendMessage(" &f| &afBoard Plugin\n");
+        Bukkit.getConsoleSender().sendMessage(" &f| Author: &aEduardo Felleto");
+        Bukkit.getConsoleSender().sendMessage(" &f| Version: &a1.0.0\n");
+        Bukkit.getConsoleSender().sendMessage(" &7-----------------------------------------\n");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Bukkit.getConsoleSender().sendMessage(" &cfBoard desligado!\n");
     }
 }
