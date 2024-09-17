8e559a459f3e1543f1ca8ed2721ea75b03dcd3a6
efelleto <eduardofelleto@icloud.com>
2024-09-16 22:31:05 -0300

    Initial commit

---
+++ src/main/java/dev/felleto/fboardmain/Fboard_main.java
@@ -0,0 +1,17 @@
+package dev.felleto.fboardmain;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Fboard_main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
