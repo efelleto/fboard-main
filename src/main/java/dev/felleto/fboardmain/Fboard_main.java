a4676199629759f460370fd527d6310c7417c418
efelleto <eduardofelleto@icloud.com>
2024-09-16 23:10:19 -0300

    [feat] assemble adapter + final commit

--- src/main/java/dev/felleto/fboardmain/Fboard_main.java
+++ src/main/java/dev/felleto/fboardmain/Fboard_main.java
@@ -1,12 +1,45 @@
 package dev.felleto.fboardmain;
 
+import io.github.thatkawaiisam.assemble.Assemble;
+import io.github.thatkawaiisam.assemble.AssembleAdapter;
+import io.github.thatkawaiisam.assemble.AssembleStyle;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public final class Fboard_main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Assemble assemble = new Assemble(this, new AssembleAdapter() {
+            @Override
+            public String getTitle(Player player) {
+                return ChatColor.AQUA + "fBoard";
+            }
+
+            @Override
+            public List<String> getLines(Player player) {
+
+                List<String> linhas = new ArrayList<>();
+
+                linhas.add("");
+                linhas.add("IGN: " + player.getDisplayName());
+                linhas.add("");
+                linhas.add(Instant.now().toString());
+                linhas.add("");
+                linhas.add(ChatColor.ITALIC + "github.com/efelleto");
+
+                return linhas;
+            }
+        });
+
+        assemble.setAssembleStyle(AssembleStyle.VIPER);
         Bukkit.getConsoleSender().sendMessage("[fBoard] Plugin iniciado");
     }
 
